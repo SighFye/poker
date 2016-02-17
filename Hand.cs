bc1989e020a23c1b41af880b259e26c78aef173a
Tom Hanson <tomhanson.home@gmail.com>
2016-02-17 23:11:06 +1000

    Update Hand.cs

--- Hand.cs
+++ Hand.cs
@@ -14,6 +14,8 @@ namespace Poker
         private List<Card> cards;
         private int maxCards;
         private int handValue = 0;
+        private string bestHandName = "";
+        private List<Card> bestHand = new List<Card>(5);
         /// <summary>
         /// Creates a hand of cards with a max number of cards it can hold
         /// </summary>
@@ -43,10 +45,69 @@ namespace Poker
             //First Check the length of the hand.
             if (cards.Count < maxCards)
             {
+                Console.WriteLine("Hand Says: Card Added");
                 cards.Add(newCard); //Add the new card to the cards array using the length as an index.
                 result = true; //The result turns to true as the add card worked.
             }
+            else
+            {
+                Console.WriteLine("Hand Says: Hand is full");
+            }
+            analyseHand();
             return result;
+        }
+        private int analyseHand()
+        {
+            
+            if (cards.Count > 4)
+            {
+                //Check for a flush
+                if (hasFlush())
+                {
+                    Console.WriteLine("Has Flush Checking For Straight");
+                }
+                else
+                {
+                    Console.WriteLine("No Flush Checking For Straight");
+                }
+            }
+            return 0;
+        }
+        private bool hasFlush()
+        {
+            //Sort all the cards based on suit
+            cards.Sort((a, b) => { return a.getSuit().CompareTo(b.getSuit()); });
+            //Loop through the cards and check their suit
+            int lastSuit = 0;
+            List<Card> potentialBestHand = new List<Card>(maxCards);
+            foreach (Card theCard in cards)
+            {
+                if (lastSuit == theCard.getSuit()) //Does the current cards suit match the last cards suit.
+                {
+                    potentialBestHand.Add(theCard); //Add the card to the potential besthand cards
+                    // We do not need to change lastSuit as this card matches the last
+                }
+                else
+                {
+                    if (potentialBestHand.Count < 5)
+                    {
+                        potentialBestHand.Clear(); //Get rid of all the cards in the best hand
+                        potentialBestHand.Add(theCard); //Add in the current card to be a part of a new best hand.
+                        lastSuit = theCard.getSuit(); //And change the suit to the current cards suit.
+                    }
+                }
+            }
+            if (potentialBestHand.Count > 4)
+            {
+                bestHand = potentialBestHand; //Since we have found a flush, this becomes our best hand.
+                bestHandName = "Flush";
+                //=========================================
+                //handValue = 0; //We'll add this in later
+                //=========================================
+                return true; //We have found a flush and have gathered all the cards matching the suit.
+            }
+            else
+                return false;
         }
     }
 }
